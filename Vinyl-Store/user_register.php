801eae161569544637e30a900c35dd9db49074a1
HansVonHin <germanyaxis12@gmail.com>
2024-09-29 04:06:34 +0800

    commit

--- Vinyl-Store/user_register.php
+++ Vinyl-Store/user_register.php
@@ -14,12 +14,22 @@ if(isset($_POST['submit'])){
 
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $username = $_POST['username'];
+   $username = filter_var($username, FILTER_SANITIZE_STRING);
    $email = $_POST['email'];
    $email = filter_var($email, FILTER_SANITIZE_STRING);
    $pass = sha1($_POST['pass']);
    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
    $cpass = sha1($_POST['cpass']);
    $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
+
+   // For profile pic, we will store the file path
+   if(isset($_FILES['profile_pic']) && $_FILES['profile_pic']['error'] == 0) {
+      $profile_pic = 'uploads/' . basename($_FILES['profile_pic']['name']);
+      move_uploaded_file($_FILES['profile_pic']['tmp_name'], $profile_pic);
+   } else {
+      $profile_pic = 'uploads/default-profile.png'; // Fallback in case no file is uploaded
+   }
 
    // reCAPTCHA Validation
    $recaptchaSecret = 'YOUR_SECRET_KEY_HERE';
@@ -31,18 +41,19 @@ if(isset($_POST['submit'])){
    if(intval($responseKeys["success"]) !== 1) {
       $message[] = 'Please complete the reCAPTCHA!';
    } else {
-      $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
-      $select_user->execute([$email]);
+      $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? OR username = ?");
+      $select_user->execute([$email, $username]);
       $row = $select_user->fetch(PDO::FETCH_ASSOC);
 
       if($select_user->rowCount() > 0){
-         $message[] = 'Email Already Exists!';
+         $message[] = 'Email or Username Already Exists!';
       } else {
          if($pass != $cpass){
             $message[] = 'Confirm Password Not Matched!';
          } else {
-            $insert_user = $conn->prepare("INSERT INTO `users`(name, email, password) VALUES(?,?,?)");
-            $insert_user->execute([$name, $email, $cpass]);
+            $created_at = date('Y-m-d H:i:s');
+            $insert_user = $conn->prepare("INSERT INTO `users`(name, username, email, password, profile_pic, created_at, updated_at) VALUES(?,?,?,?,?,?,?)");
+            $insert_user->execute([$name, $username, $email, $cpass, $profile_pic, $created_at, $created_at]);
             $message[] = 'Registered Successfully!';
          }
       }
@@ -72,16 +83,20 @@ if(isset($_POST['submit'])){
    </div>
    
    <div class="image-container">
-      <img src="images/login-banner.jpg" alt="Branding Image">
+      <img src="images/login-banner.jpg" alt="Register Branding">
    </div>
 
    <section class="form-container">
-      <form action="" method="post">
-         <h3>Register Now</h3>
-         <input type="text" name="name" required placeholder="Enter Your Username" maxlength="20" class="box">
+      <form action="" method="post" enctype="multipart/form-data">
+         <h3>Create Your Account</h3>
+         <input type="text" name="name" required placeholder="Enter Your Full Name" maxlength="20" class="box">
+         <input type="text" name="username" required placeholder="Choose a Username" maxlength="20" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
          <input type="email" name="email" required placeholder="Enter Your Email" maxlength="50" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
          <input type="password" name="pass" required placeholder="Enter Your Password" maxlength="20" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
          <input type="password" name="cpass" required placeholder="Confirm Your Password" maxlength="20" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+
+         <!-- Profile Picture Input -->
+         <input type="file" name="profile_pic" accept="image/*" class="box" placeholder="Upload Profile Picture">
          
          <!-- Google reCAPTCHA -->
          <div class="g-recaptcha" data-sitekey="your-site-key"></div>
